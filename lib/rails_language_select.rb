c36fe9a1ac655c591de0f1ca6d4f81ff264e1102
Kaspars <kasparsj@gmail.com>
2017-02-23 19:58:57 +0200

    more renaming

---
+++ lib/rails_language_select.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+require 'i18n_data'
+require 'sort_alphabetical'
+
+require 'rails_language_select/version'
+require 'rails_language_select/formats'
+require 'rails_language_select/tag_helper'
+
+if defined?(ActionView::Helpers::Tags::Base)
+  require 'rails_language_select/language_select_helper'
+else
+  require 'rails_language_select/rails3/language_select_helper'
+end
